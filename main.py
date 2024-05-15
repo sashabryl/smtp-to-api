5b921b86b706a2a1c0a7cb6574dc5dd683ec484d
Olexander Bryl <edlrian814@gmail.com>
2024-05-15 20:54:06 +0300

    Fix minor bug

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ app = FastAPI()
 RESEND_API_URL = "https://api.resend.com/emails"
 RESEND_API_KEY = os.getenv("RESEND_API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
+SENDER = os.getenv("SENDER")
 
 
 @app.post("/forward-email")
 async def forward_email(request: Request):
     email_data = await request.json()
+    email_data["from"] = SENDER
     authentication_header = request.headers.get("Authorization")
     if authentication_header.split()[1] != SECRET_KEY:
         return {"status": 401, "message": "Unauthorized"}
   
-    response = await requests.post(
+    response = requests.post(
       RESEND_API_URL,
       json=email_data,
       headers={
